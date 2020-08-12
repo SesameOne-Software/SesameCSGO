c3f26425a19fb98e98bf43153cc90c013f9b2d6d
peatreat <33198834+peatreat@users.noreply.github.com>
2020-08-12 14:46:58 -0500

    loader data crash fixes

--- csgo/security/text_section_hasher.hpp
+++ csgo/security/text_section_hasher.hpp
@@ -13,13 +13,6 @@ namespace anti_patch {
 	extern uintptr_t g_text_section;
 	extern uintptr_t g_text_section_size;
 
-	struct s_header_data {
-		size_t m_num_of_sections;
-		std::vector< IMAGE_SECTION_HEADER > m_sections;
-	};
-
-	extern s_header_data g_header_data;
-
 	typedef struct {
 		LPVOID lpVirtualAddress;
 		DWORD dwSizeOfRawData;
@@ -30,24 +23,12 @@ namespace anti_patch {
 		SECTIONINFO SectionInfo;
 	} HASHSET, * PHASHSET;
 
-	__forceinline int GetTextSectionInfo( uintptr_t lpModBaseAddr ) {
-		for ( int i = 0; i < anti_patch::g_header_data.m_num_of_sections; ++i ) {
-			if ( !strcmp( ( char* ) anti_patch::g_header_data.m_sections [ i ].Name, _( ".text" ) ) ) {
-				g_text_section = ( uintptr_t ) ( ( DWORD64 ) lpModBaseAddr + anti_patch::g_header_data.m_sections [ i ].VirtualAddress );
-				g_text_section_size = anti_patch::g_header_data.m_sections [ i ].SizeOfRawData;
-				break;
-			}
-		}
-
-		return 0;
-	}
-
-	__forceinline DWORD64 HashSection( ) {
+	__forceinline DWORD64 HashSection() {
 		DWORD64 hash = 0;
 
-		for ( int i = 0; i < g_text_section_size; ++i ) {
-			if ( *reinterpret_cast< uint8_t* > ( g_text_section + i ) ) {
-				hash = *reinterpret_cast< uint8_t* > ( g_text_section + i ) + ( hash << 6 ) + ( hash << 16 ) - hash;
+		for (int i = 0; i < g_text_section_size; ++i) {
+			if (*reinterpret_cast<uint8_t*> (g_text_section + i)) {
+				hash = *reinterpret_cast<uint8_t*> (g_text_section + i) + (hash << 6) + (hash << 16) - hash;
 			}
 		}
 
