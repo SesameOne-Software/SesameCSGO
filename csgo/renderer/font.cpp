607a7673d19b3d7b0c9e10da2b81804a4ed3e49f
ses <sesame03@protonmail.com>
2020-09-21 20:50:52 -0400

    truetype font renderer

--- csgo/renderer/font.cpp
+++ csgo/renderer/font.cpp
@@ -1,71 +1,110 @@
+#include <shlobj_core.h>
+#include <vector>
+
+#include "../sdk/sdk.hpp"
 #include "font.hpp"
 
 #define STB_TRUETYPE_IMPLEMENTATION
 #include "../stb/stb_truetype.h"
 
-std::optional<std::unique_ptr<font::font>> font::add_font_from_memory( const std::vector<uint8_t>& data, std::string_view name, int size ) {
-	auto new_font = std::make_unique<font> ( );
+ID3DXSprite *font_sprite = nullptr;
 
-	new_font->name = name;
-	new_font->size = size;
+struct font_vertex_t
+{
+	float x, y, z, rhw;
+	uint32_t color;
+	float tx, ty;
+};
 
-	const auto ttf_size = data.size ( );
+std::optional<truetype::font> truetype::create_font(const std::vector<uint8_t> &font_data, std::string_view font_name, float size)
+{
+	if (stbtt_fontinfo font_info; stbtt_InitFont(&font_info, font_data.data(), 0))
+		return font{font_name.data(), size, font_info};
 
-	/* prepare font */
-	stbtt_fontinfo info;
+	return {};
+}
 
-	if ( !stbtt_InitFont ( &info, data.data ( ), 0 ) )
-		return {};
+IDirect3DTexture9 *truetype::font::create_texture(const std::string &string)
+{
+	constexpr auto bitmap_width = 512;
 
-	int b_w = 512; /* bitmap width */
-	int b_h = 128; /* bitmap height */
-	int l_h = 64;  /* line height */
+	auto bitmap = std::make_unique<std::uint8_t[]>(bitmap_width * static_cast<int>(size + 0.5f));
 
-	/* create a bitmap for the phrase */
-	unsigned char *bitmap = calloc(b_w * b_h, sizeof(unsigned char));
+	auto scale = stbtt_ScaleForPixelHeight(&font_info, size);
 
-	/* calculate font scaling */
-	float scale = stbtt_ScaleForPixelHeight(&info, l_h);
+	auto x = 0, ascent = 0, descent = 0, lineGap = 0;
+	stbtt_GetFontVMetrics(&font_info, &ascent, &descent, &lineGap);
 
-	char *word = "the quick brown fox";
+	ascent *= scale;
+	descent *= scale;
 
-	int x = 0;
+	for (auto i = 0; i < string.size(); ++i)
+	{
+		auto c_x1 = 0, c_y1 = 0, c_x2 = 0, c_y2 = 0;
+		stbtt_GetCodepointBitmapBox(&font_info, string[i], scale, scale, &c_x1, &c_y1, &c_x2, &c_y2);
 
-	int ascent, descent, lineGap;
-	stbtt_GetFontVMetrics(&info, &ascent, &descent, &lineGap);
+		auto y = ascent + c_y1;
 
-	ascent = roundf(ascent * scale);
-	descent = roundf(descent * scale);
+		auto byteOffset = x + (y * bitmap_width);
+		stbtt_MakeCodepointBitmap(&font_info, bitmap.get() + byteOffset, c_x2 - c_x1, c_y2 - c_y1, bitmap_width, scale, scale, string[i]);
 
-	csgo::i::dev->CreateVertexBuffer ( sizeof ( font_vertex_t ) * 12, NULL, D3DFVF_XYZRHW | D3DFVF_DIFFUSE | D3DFVF_TEX1, D3DPOOL_MANAGED, &vertex_buffer, nullptr );
-
-	for (auto i = 0; i < strlen(word); ++i) {
-		/* get bounding box for character (may be offset to account for chars that dip above or below the line */
-		int c_x1, c_y1, c_x2, c_y2;
-		stbtt_GetCodepointBitmapBox ( &info, word [ i ], scale, scale, &c_x1, &c_y1, &c_x2, &c_y2 );
-
-		/* compute y (different characters have different heights */
-		int y = ascent + c_y1;
-
-		/* render character (stride and offset is important here) */
-		int byteOffset = x + ( y * b_w );
-		stbtt_MakeCodepointBitmap ( &info, bitmap + byteOffset, c_x2 - c_x1, c_y2 - c_y1, b_w, scale, scale, word [ i ] );
-
-		/* how wide is this character */
-		int ax;
-		stbtt_GetCodepointHMetrics ( &info, word [ i ], &ax, 0 );
+		auto ax = 0;
+		stbtt_GetCodepointHMetrics(&font_info, string[i], &ax, 0);
 		x += ax * scale;
 
-		/* add kerning */
-		int kern;
-		kern = stbtt_GetCodepointKernAdvance ( &info, word [ i ], word [ i + 1 ] );
+		auto kern = stbtt_GetCodepointKernAdvance(&font_info, string[i], string[i + 1]);
 		x += kern * scale;
 	}
 
-	/* save out a 1 channel image */
-	stbi_write_png("out.png", b_w, b_h, 1, bitmap, b_w);
+	IDirect3DTexture9 *texture = nullptr;
 
-	free(bitmap);
+	if (D3DXCreateTexture(csgo::i::dev, bitmap_width, static_cast<int>(size + 0.5f), 1, D3DUSAGE_DYNAMIC, D3DFMT_A8R8G8B8, D3DPOOL_DEFAULT, &texture) < 0)
+		return nullptr;
 
-	return new_font;
+	D3DLOCKED_RECT rect;
+
+	if (texture->LockRect(0, &rect, nullptr, 0) < 0)
+		return nullptr;
+
+	memcpy(rect.pBits, bitmap.get(), bitmap_width * static_cast<int>(size + 0.5f));
+
+	texture->UnlockRect(0);
+
+	return texture;
+}
+
+void truetype::font::draw_text(float x, float y, const std::string &string, uint32_t color, text_flags_t flags)
+{
+	auto texture = create_texture(string);
+
+	if (!texture || !font_sprite || font_sprite->Begin(D3DXSPRITE_ALPHABLEND) < 0)
+	{
+		if (texture)
+		{
+			texture->Release();
+			texture = nullptr;
+		}
+
+		return;
+	}
+
+	font_sprite->Draw(texture, nullptr, nullptr, &D3DXVECTOR3{x, y, 0.0f}, color);
+	font_sprite->End();
+
+	texture->Release();
+	texture = nullptr;
+}
+
+void truetype::begin()
+{
+	D3DXCreateSprite(csgo::i::dev, &font_sprite);
+}
+
+void truetype::end()
+{
+	if (font_sprite)
+	{
+		font_sprite->Release();
+		font_sprite = nullptr;
+	}
 }
--- csgo/renderer/font.hpp
+++ csgo/renderer/font.hpp
@@ -1,50 +1,36 @@
 #pragma once
-#include <cstdint>
-#include <string_view>
-#include <functional>
-#include <memory>
-#include <optional>
 #include <d3d9.h>
 #include <d3dx9.h>
-#include "../sdk/sdk.hpp"
+#include <string>
+#include <map>
+#include <optional>
+#include <memory>
 
-namespace font {
-	struct glyph_vec {
+namespace truetype
+{
+	enum text_flags_t
+	{
+		text_flags_none = 0,
+		text_flags_dropshadow,
+		text_flags_outline
+	};
+
+	struct font_vec_t
+	{
 		float x, y;
 	};
 
-	struct glyph_info {
-		glyph_vec size;
-		glyph_vec bearing;
-		uintptr_t advance;
-		IDirect3DTexture9* texture;
-		bool colored;
+	struct font
+	{
+		std::string font_name;
+		float size;
+		stbtt_fontinfo font_info;
+
+		IDirect3DTexture9 *create_texture(const std::string &string);
+		void draw_text(float x, float y, const std::string &string, uint32_t color, text_flags_t flags);
 	};
 
-	struct font {
-		std::string name;
-		int size;
-		IDirect3DVertexBuffer9* vertex_buffer;
-		std::map<int, glyph_info> glyph_map;
-
-		void on_lost_device ( ) {
-			if ( vertex_buffer )
-				vertex_buffer->Release ( );
-
-			vertex_buffer = nullptr;
-		}
-
-		void on_reset_device ( ) {
-			if (!vertex_buffer )
-				csgo::i::dev->CreateVertexBuffer ( sizeof ( font_vertex_t ) * 12, NULL, D3DFVF_XYZRHW | D3DFVF_DIFFUSE | D3DFVF_TEX1, D3DPOOL_MANAGED, &vertex_buffer, nullptr );
-		}
-	};
-
-	struct font_vertex_t {
-		float x, y, z, rhw;
-		uint32_t color;
-		float tx, ty;
-	};
-
-	std::optional<std::unique_ptr<font>> add_font_from_memory( const std::vector<uint8_t>& data, std::string_view name, int size );
-}
+	std::optional<font> create_font(const std::vector<uint8_t> &font_data, std::string_view font_name, float size);
+	void begin();
+	void end();
+} // namespace truetype
